1ee6357a8f99042ecbf336ace1dc76bb456d255a
Abi-12-08 <abi.viji8006@gmail.com>
2026-03-13 14:18:34 +0530

    Base application setup

---
+++ BookMyStayApp/src/BookMyStayApp.java.java
@@ -0,0 +1,30 @@
+/**
+ * BookMyStayApp
+ *
+ * Entry point for the Hotel Booking Management System.
+ * This application demonstrates how a Java program begins execution
+ * and prints a welcome message with application information.
+ *
+ * @author Jai Aaditya
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    /**
+     * Main method – JVM execution starts here
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println(" Welcome to BookMyStay");
+        System.out.println(" Hotel Booking Management System");
+        System.out.println(" Version : 1.0");
+        System.out.println("=======================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using BookMyStay!");
+    System.out.println("Application: Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+    }
+}
